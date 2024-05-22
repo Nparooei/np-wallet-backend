64a37f596a21e2b59050327c5a6ad3325837f926
Nasser <nparooei@outlook.com>
2024-05-22 19:55:28 +0200

    test: added tests

--- test/wallet_backend_type_script.test.ts
+++ test/wallet_backend_type_script.test.ts
@@ -1,17 +1,30 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as WalletBackendTypeScript from '../lib/wallet_backend_type_script-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as WalletBackendTypeScript from '../lib/wallet_backend_type_script-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/wallet_backend_type_script-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new WalletBackendTypeScript.WalletBackendTypeScriptStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Cognito User Pool Created', () => {
+  const app = new cdk.App();
+  const stack = new WalletBackendTypeScript.CognitoUserPoolStack(app, 'cognito-user-pool-test-stack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    template.hasResource('AWS::Cognito::UserPool', {
+        "DeletionPolicy": "Retain",
+        "UpdateReplacePolicy": "Retain",
+        "Properties": {
+            "Policies": {
+            "PasswordPolicy": {
+                "MinimumLength": 8,
+                "RequireLowercase": false,
+                "RequireSymbols": false,
+                "RequireUppercase": false
+            }
+            }
+        }
+        }
+        
+    );
+  
+  template.resourceCountIs('AWS::Cognito::UserPool', 1); 
+  template.resourceCountIs('AWS::Cognito::UserPoolClient', 1); 
+  template.resourceCountIs('AWS::Amplify::App', 1); 
 });
